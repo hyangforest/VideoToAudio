8d8f5c9f21b4cd7f0d7dd23c88808a510b3f35cb
Hyangrim Lee <hyangrim.lee@kims.co.kr>
2023-07-31 08:42:36 +0900

    파일 정보 있을 경우 버튼 색 변경

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Diagnostics;
 using System.Windows.Navigation;
 using System.Windows.Documents;
 using System.Linq;
+using System.Windows.Media;
+using System.Windows.Controls;
 
 namespace VideoToAudio
 {
@@ -59,6 +61,8 @@ namespace VideoToAudio
         /// <param name="e"></param>
         private void btnFindVideo_Click(object sender, RoutedEventArgs e)
         {
+            videoFilePath = string.Empty;
+
             // 폴더 선택 대화 상자를 열고, 사용자가 폴더를 선택하도록 합니다.
             var openFileDialog = new OpenFileDialog
             {
@@ -67,7 +71,6 @@ namespace VideoToAudio
                 Filter = "Video Files|*.mp4;*.avi;*.mkv;*.wmv;*.flv;*.mov;*.mpeg|All Files|*.*",
                 FilterIndex = 1 
             };
-
 
             if (openFileDialog.ShowDialog() == true)
             {
@@ -85,6 +88,26 @@ namespace VideoToAudio
                 else
                 {
                     MessageBox.Show("파일 정보를 가져올 수 없습니다.", "Video 파일 열기");
+                }
+            }
+
+
+            // 버튼 색깔 바꾸기
+            Border? borderElement = btnFindVideo.Template.FindName("borderBtnFindVideo", btnFindVideo) as Border;
+
+            if (borderElement != null)
+            {
+                Color color;
+
+                if (!string.IsNullOrEmpty(videoFilePath))
+                {
+                    color = (Color)ColorConverter.ConvertFromString("#9ACD32");
+                    borderElement.Background = new SolidColorBrush(color);
+                }
+                else
+                {
+                    color = (Color)ColorConverter.ConvertFromString("#FFF2BE80");
+                    borderElement.Background = new SolidColorBrush(color);
                 }
             }
         }
